ec9de191c0cb1dff6cd870d677d629afd0249537
gianlucatuono <gianlucatuono@libero.it>
2020-08-25 12:42:01 +0200

    s

---
+++ Common/dataMailXXX.php
@@ -0,0 +1,26 @@
+<?php
+
+return $config = [
+    "setting" => [
+        "smtpHost" => "XXX",
+        "usernameProvider" => "XXX",
+        "passwordProvider" => "XXX",
+        "port1" => "XXX",
+        "port2" => "XXX"
+    ],
+    "address" => [
+        "XXX" => "XXX",
+        "XXX" => "XXX",
+        "XXX" => "XXX",
+        "XXX" => "XXX",
+        "XXX" => "XXX",
+        "XXX" => "XXX",
+        "XXX" => "XXX",
+        "XXX" => "XXX"
+    ],
+    "body" => [
+        "subject" => "Avviso scadenza Veicolo - Ilmioportale.cloud informa",
+        "messageHtml" => "<h2>Controllare lo stato delle scadenze dei veicoli</h2><br><h5>Questa e' una mail informativa autogenerata, si prega di non rispondere, grazie <b><br>L'amministratore Gianluca Tuono</b>",
+        "messageOther" => "Controllare lo stato delle scadenze dei veicoli"
+    ]
+];
